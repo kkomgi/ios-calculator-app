c72bd23f24583f2060e3fd878dea1218cafdadbf
PrismSpirit <yeolkeut@gmail.com>
2024-02-20 16:31:41 +0900

    Make basic connection with Storyboard

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var expression = ""
+    var `operator`: Operator?
+    var operand = "0"
+    var isOperatorActivated = false
+    var errorHasOccured = false
+    
+    @IBOutlet var operatorLabel: UILabel!
+    @IBOutlet var operandLabel: UILabel!
+    
+    @IBOutlet var logsStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func acButtonTouchedUP(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func ceButtonTouchedUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signToggleButtonTouchedUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operatorButtonTouchedUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func calculateButtonTouchedUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func dotButtonTouchedUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func numberButtonTouchedUp(_ sender: UIButton) {
+        
+    }
 }
-
